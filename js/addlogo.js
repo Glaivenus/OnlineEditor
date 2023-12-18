f671992a63e1671ee711ba3a25044105efde99ba
Glaivenus <58286766+Glaivenus@users.noreply.github.com>
2023-12-18 10:31:30 +0800

    Add files via upload

---
+++ js/addlogo.js
@@ -0,0 +1,17 @@
+// function addLogoToCanvas() {
+
+//     var logoPath = '../img/logo1.png';
+
+//     fabric.Image.fromURL(logoPath, function (img) {
+//         img.set({
+//             scaleX: 0.08, 
+//             scaleY: 0.08,
+//             left: 100, 
+//             top: canvas.width / 8,
+//         });
+
+//         canvas.add(img);
+//         canvas.bringToFront(img);
+//     });
+// }
+
